b6c2aec0cde1ddb17a7a49a3126f6807d76941a6
alireza <a.rouhani@dinadev.ir>
2022-11-28 00:18:23 +0330

    add new setter/getter for User property

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,26 +2,24 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ *
+ * @package App\Models
+ *
+ * @property int id
+ * @property string email
+ * @property string password
+ * @property string|null card_number
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +39,69 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail(string $email): User
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return User
+     */
+    public function setPassword(string $password): User
+    {
+        $this->password = bcrypt($password);
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCardNumber(): ?string
+    {
+        return $this->card_number;
+    }
+
+    /**
+     * @param string|null $card_number
+     * @return User
+     */
+    public function setCardNumber(?string $card_number): User
+    {
+        $this->card_number = $card_number;
+
+        return $this;
+    }
 }
