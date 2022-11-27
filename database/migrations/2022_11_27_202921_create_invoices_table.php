a5143822757af0b1069871369e1be3c32cea86b5
alireza <a.rouhani@dinadev.ir>
2022-11-28 00:16:53 +0330

    create invoices migration

---
+++ database/migrations/2022_11_27_202921_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Constants\InvoiceConstants;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('number', 6)->unique();
+            $table->enum('status', InvoiceConstants::STATUSES)->default(InvoiceConstants::INIT);
+            $table->unsignedBigInteger('final_price')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
