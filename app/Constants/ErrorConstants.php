5d0d8f89b06697931721f45d18fa8b56b600e8ec
alireza <a.rouhani@dinadev.ir>
2022-11-28 18:07:43 +0330

    add new error code

--- app/Constants/ErrorConstants.php
+++ app/Constants/ErrorConstants.php
@@ -4,8 +4,9 @@ namespace App\Constants;
 
 class ErrorConstants
 {
+    const UNKNOWN_VALUE = 1001;
 
     const LABELS = [
-
+        self::UNKNOWN_VALUE => 'Amount value should be an integer.',
     ];
 }
