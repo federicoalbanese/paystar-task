1cf0978414149cb8d1c1919237fa650b2968a950
alireza <a.rouhani@dinadev.ir>
2022-11-28 09:43:23 +0330

    create payments table migration

---
+++ database/migrations/2022_11_28_060237_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->constrained('invoices');
+            $table->string('transaction_id');
+            $table->string('reference_id')->nullable();
+            $table->string('status');
+            $table->unsignedBigInteger('amount');
+            $table->string('card_number')->nullable();
+            $table->timestamp('paid_at')->nullable();
+            $table->string('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
