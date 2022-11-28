e4e81dd20330d8703bb3f0ce4abc23f0c58012e7
alireza <a.rouhani@dinadev.ir>
2022-11-28 18:08:05 +0330

    add new const for http methods

--- app/Constants/PaymentConstants.php
+++ app/Constants/PaymentConstants.php
@@ -10,6 +10,15 @@ class PaymentConstants
 
     const FAIL = 'FAIL';
 
+    const GET = 'GET';
+
+    const POST = 'POST';
+
+    const HTTP_TYPES = [
+        self::GET => 1,
+        self::POST => 0,
+    ];
+
     const STATUSES = [
         self::INIT,
         self::SUCCESS,
