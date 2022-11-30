9c57f939f06fa264da5db2af595852af93f22796
alireza <a.rouhani@dinadev.ir>
2022-11-30 22:45:46 +0330

    add new error code and message

--- app/Constants/ErrorConstants.php
+++ app/Constants/ErrorConstants.php
@@ -6,7 +6,10 @@ class ErrorConstants
 {
     const UNKNOWN_VALUE = 1000;
 
+    const INVOICE_NOTFOUND = 1001;
+
     const LABELS = [
         self::UNKNOWN_VALUE => 'Amount value should be an integer.',
+        self::INVOICE_NOTFOUND => 'Invoice not selected or does not exist.',
     ];
 }
