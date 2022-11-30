049f92831ea8c1fee13110d7f240b42a2c87ecac
alireza <a.rouhani@dinadev.ir>
2022-11-30 22:50:41 +0330

    change extended exception base

--- app/Services/IPG/Exceptions/InvoiceNotFoundException.php
+++ app/Services/IPG/Exceptions/InvoiceNotFoundException.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\IPG\Exceptions;
 
-class InvoiceNotFoundException extends PaymentException
+use App\Exceptions\LogicException;
+
+class InvoiceNotFoundException extends LogicException
 {
 }
