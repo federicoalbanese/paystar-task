d165e625f8f558c56801edd6bb95c0f5239a5b48
alireza <a.rouhani@dinadev.ir>
2022-12-01 16:12:46 +0330

    create blade for fail and success payment happen

---
+++ resources/views/payments/fail-payment.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-red-600 dark:text-red-400 leading-tight">
+            {{ __('Fail payment') }}
+        </h2>
+    </x-slot>
+    <div class="container mx-auto mt-5">
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">message</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{$payment->getMessage()}}</dd>
+        </div>
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">status</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{$payment->getStatus()}}</dd>
+        </div>
+    </div>
+</x-app-layout>
