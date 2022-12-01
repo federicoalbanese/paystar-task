1b9d6da1f9898214b7d5afeffb2698937b9848ba
alireza <a.rouhani@dinadev.ir>
2022-12-01 13:36:04 +0330

    add new error code and message

--- app/Constants/ErrorConstants.php
+++ app/Constants/ErrorConstants.php
@@ -8,8 +8,11 @@ class ErrorConstants
 
     const INVOICE_NOTFOUND = 1001;
 
+    const CARD_NUMBER_NOT_MATCH = 1002;
+
     const LABELS = [
         self::UNKNOWN_VALUE => 'Amount value should be an integer.',
         self::INVOICE_NOTFOUND => 'Invoice not selected or does not exist.',
+        self::CARD_NUMBER_NOT_MATCH => 'card number paying with not match with your card in website.',
     ];
 }
