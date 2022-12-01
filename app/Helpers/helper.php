88ff4e0b85e5596eaa55a652e1c4d4453713dbef
alireza <a.rouhani@dinadev.ir>
2022-12-01 10:38:36 +0330

    add generateRandomString helper

---
+++ app/Helpers/helper.php
@@ -0,0 +1,20 @@
+<?php
+
+if (! function_exists('generateRandomString')) {
+    /**
+     * @param integer $length
+     *
+     * @return string
+     */
+    function generateRandomString(int $length = 6): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
+}
